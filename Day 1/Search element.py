3ab6095111ceefc9e72a9ab4456865d196bb86b6
Aarya <24khobaragadea@rbunagpur.in>
2026-06-15 20:11:12 +0530

    Added counting any one num.py

--- Day 1/Search element.py
+++ Day 1/Search element.py
@@ -1,11 +1,12 @@
 arr=[6,5,4,3,2,7]
-target=9
+target=3
 found=False
 index=0
 for num in arr:
     if num==target:
         found=True
-    
+        print(index)
+    index=index+1
 if found:
     print("element found")
 else:
---
+++ Day 1/counting any one num.py
@@ -0,0 +1,8 @@
+arr=[1,2,1,3,1,4,1]
+
+target=4
+count=0
+for num in arr:
+    if num==target:
+        count=count+1
+print(count)
